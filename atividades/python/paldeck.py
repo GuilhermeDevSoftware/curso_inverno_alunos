271124d777c98db672e754ba5d5716373396331b
Guilherme Costa <guizeracostasilva@outlook.com>
2024-07-23 21:12:34 -0300

    create paldeck

--- atividades/python/paldeck.py
+++ atividades/python/paldeck.py
@@ -1,1 +1,5 @@
-print('Ola')
+nome = input("Digite seu nome: ")
+print("Pawl dex de " + nome)
+
+quantidade_pawl = input("Quantos pawls você tem: ")
+print(f"{nome} possuiu {quantidade_pawl} pawls")
